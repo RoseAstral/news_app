b06c424b894ce7c4d27ca8c16183d4a6643e7920
RoseAstral <lilianascarletaurora@gmail.com>
2025-07-23 13:09:48 +0200

    Update settings.py

--- news_app/news_app/settings.py
+++ news_app/news_app/settings.py
@@ -80,11 +80,15 @@ WSGI_APPLICATION = "news_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'DatabaseName', 
+        'USER': 'username', 
+        'PASSWORD': 'password', 
+        'HOST': 'localhost', 
+        'PORT': '', 
+    } 
 }
 
 
